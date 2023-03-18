55208b2c798615262d1a4cc9b8ca24b6d83dc3e6
Paranoid Rabbit <a_quaker@hotmail.com>
2023-03-18 22:01:25 +0100

    Add win-condition

--- README.md
+++ README.md
@@ -7,7 +7,6 @@ Requires [NPM](https://docs.npmjs.com/downloading-and-installing-node-js-and-npm
 Run `live-server` from root-folder
 
 # Things left
-* Victory condition
 * Sound system
 * More stages/enemies & gameplay
 * Powerups?
--- stages/stage_1.js
+++ stages/stage_1.js
@@ -10,13 +10,18 @@ class BaseStage {
         this.pc.set_stage(this);
         this.bullets = [];
         this.baddies = [];
+        this.spawn_baddies = [];
         this.effects = [];
         this.hp = new HealthBar(this.pc);
         this.frame = new Framing();
-        this.failed = 60;
+        this.failed = 100;
         this.failing = 0;
         this.failed_text = new TextHandler(" broke down", 55, 250, 4);
+        this.won = 100;
+        this.winning = 0;
+        this.won_text = new TextHandler(" victory", 75, 250, 4);
         this.new_stage = false;
+        this.timer = 0;
     }
 
     draw(ctx) {
@@ -60,14 +65,33 @@ class BaseStage {
             this.failed_text.draw(ctx);
             ctx.closePath();
         }
+
+        if (this.winning >= 1) {
+            ctx.beginPath();
+            ctx.strokeStyle = "#FF00FF";
+            this.won_text.draw(ctx);
+            ctx.closePath();
+        }
     }
 
     action(controls) {
-        if (this.failing < this.failed) {
+        if (this.failing < this.failed & this.winning < this.won) {
+            this.timer += 1;
+
             if (this.pc.think(controls)) {
                 this.failing += 1;
             } else {
                 this.failing = 0;
+            }
+
+            if (this.baddies.length <= 0 & this.spawn_baddies.length <= 0) {
+                this.winning += 1;
+            } else {
+                this.winning = 0;
+            }
+
+            while(0 < this.spawn_baddies.length && this.timer >= this.spawn_baddies[0][0]) {
+                this.add_baddie(this.spawn_baddies.shift()[1]);
             }
 
             for (var i = this.bullets.length - 1; i >= 0; i--) {
@@ -100,7 +124,11 @@ class BaseStage {
                 }
             }
         } else {
-            this.new_stage = new MainMenu(new PC());
+            if (this.failing >= this.failed) {
+                this.new_stage = new MainMenu(new PC());
+            } else if (this.winning >= this.won) {
+                this.new_stage = this.get_next_stage();
+            }
         }
     }
 
@@ -126,18 +154,29 @@ export class Stage1 extends BaseStage {
         super(pc);
         this.spawn_baddies = [
             [100, new ShipAngy(150, 0, this.pc, this)],
-            [200, new ShipAngy(150, 0, this.pc, this)]
+            [200, new ShipAngy(150, 0, this.pc, this)],
+            [300, new ShipAngy(75, 0, this.pc, this)],
+            [300, new ShipAngy(225, 0, this.pc, this)]
         ];
-        this.timer = 0;
     }
 
-    action(controls) {
-        super.action(controls);
+    get_next_stage() {
+        return new Stage2(this.pc);
+    }
+}
 
-        this.timer += 1;
+export class Stage2 extends BaseStage {
+    constructor(pc) {
+        super(pc);
+        this.spawn_baddies = [
+            [100, new ShipAngy(75, 0, this.pc, this)],
+            [100, new ShipAngy(225, 0, this.pc, this)],
+            [150, new ShipAngy(75, 0, this.pc, this)],
+            [150, new ShipAngy(225, 0, this.pc, this)]
+        ];
+    }
 
-        while(0 < this.spawn_baddies.length && this.timer >= this.spawn_baddies[0][0]) {
-            this.add_baddie(this.spawn_baddies.shift()[1]);
-        }
+    get_next_stage() {
+        return new Stage1(this.pc);
     }
 }
